acba9e25042b2d65c75b3f54e991440b27a39e62
Ma-Namba <m.namba.032016@gmail.com>
2026-09-12 09:03:21 +0900

    未実装ページを実装

---
+++ resources/views/errors/501.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div style="text-align: center; padding: 50px 20px;">
+    <h1 style="color: #4a5568; font-size: 2rem; margin-bottom: 1rem;">
+        この機能は未実装です
+    </h1>
+</div>
+@endsection
